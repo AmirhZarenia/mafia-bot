515ebb72ea4f601c118cfd45073650f1d2cb8893
AmirHossin Zarenia <89810392+AmirhZarenia@users.noreply.github.com>
2025-11-26 14:10:19 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    telegramToken: process.env.TELEGRAM_TOKEN || 'YOUR_BOT_TOKEN_HERE',
+    telegramToken: process.env.TELEGRAM_TOKEN || '1027881411:AAHsqhzx4UGJEY6qOSJydLopHQw66Sxw0xg',
     mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/mafia-game',
     adminIds: [123456789], // ایدی عددی ادمین‌ها
     port: process.env.PORT || 3000
